bc23ff1af001a085e47587b5e012a90b34dd5c7d
karimohamed <karimohamed2002@gmail.com>
2023-10-11 01:04:41 +0300

    changes the port of react app's url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@ Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "http://localhost:3001/"
+        # where your request comes from react app's IP address or URL
+        origins "http://127.0.0.1:5173/"
         
         resource "*",
                 headers: :any,
