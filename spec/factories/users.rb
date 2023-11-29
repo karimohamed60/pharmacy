39c7db02d8856e51f93a4d0525d442f8a0e00b22
karimohamed <karimohamed2002@gmail.com>
2023-11-29 13:40:42 +0200

    add fake data to users test model by factorybot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
     factory :user do
-
+        username  { 'karimohamed' }
+        full_name { 'Karim Mohamed Mahmoud' }
+        role      { association :role, role_name: 'inventory' }
+        password  { '123456789' }
+        
     end
 end
-  
