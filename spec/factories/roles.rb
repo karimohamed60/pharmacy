12554afad4e99e539f246dea987a35e3322e4356
karimohamed <karimohamed2002@gmail.com>
2023-11-29 13:40:33 +0200

    add fake data to roles test model by factorybot

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
     factory :role do
+        role_name { 'admin' }
+    end
 
+    factory :inventory_role, parent: :role do
+        role_name { 'inventory' }
     end
 end
-  
