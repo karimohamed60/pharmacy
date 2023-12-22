61ba897bc739a7eaae0976059df757ba2eb4b344
karimohamed <karimohamed2002@gmail.com>
2023-12-22 03:00:43 +0200

    manage auth token and local storage

---
+++ client/src/services/authService.js
@@ -0,0 +1,24 @@
+import Cookies from 'js-cookie';
+
+const setAuthTokenCookie = (token) => {
+  Cookies.set('token', token, { expires: 1 / 48 }); // , secure: true option for HTTPS only
+};
+
+const getAuthTokenCookie = () => {
+  return Cookies.get('token') || null;
+};
+
+const clearAuthTokenCookie = () => {
+  Cookies.remove('token');
+};
+
+export const clearLocalStorage = () => {
+  localStorage.clear();
+};
+
+const isAuthenticated = () => {
+  return getAuthTokenCookie();
+};
+
+
+export { setAuthTokenCookie, getAuthTokenCookie, clearAuthTokenCookie, isAuthenticated};
