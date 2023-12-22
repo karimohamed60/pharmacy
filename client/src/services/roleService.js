13fbf4efe2de51c8cdb7304da393b6407ff504c7
karimohamed <karimohamed2002@gmail.com>
2023-12-22 03:00:09 +0200

    add role_service for managing role of user

---
+++ client/src/services/roleService.js
@@ -0,0 +1,9 @@
+const setUserRole = (role) => {
+    localStorage.setItem("role", role);
+};
+
+const getUserRole = () => {
+    return localStorage.getItem("role");
+};
+
+export { setUserRole, getUserRole };
