f2e6008654b1315edf427c3310f61030ba3efeba
karimohamed <karimohamed2002@gmail.com>
2023-12-22 02:55:56 +0200

    redirect unauthenticated users to login

---
+++ client/src/routes/PrivateRoutes.jsx
@@ -0,0 +1,12 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import { getAuthTokenCookie} from '../services/authService';
+
+const PrivateRoutes = () => {
+  const token = getAuthTokenCookie();
+
+  return(
+    token ? <Outlet/> : <Navigate to="/"/>
+  )
+}
+
+export default PrivateRoutes;
