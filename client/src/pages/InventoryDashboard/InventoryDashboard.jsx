118bb47c992b0cbee115c449057dd29678168a73
AhmedGomaa15 <ahmedrooney42@gmal.com>
2024-01-19 20:51:11 +0200

    modify styles in login and app

--- client/src/pages/InventoryDashboard/InventoryDashboard.jsx
+++ client/src/pages/InventoryDashboard/InventoryDashboard.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import LogoutButton from '../../auth/LogoutButton';
-
+import './InventoryDashboard.css'
 const InventoryDashboard = () => {
 
+    
     return(
         <>
+    
             Hello in Inventory
             <div>
                 <Outlet />
