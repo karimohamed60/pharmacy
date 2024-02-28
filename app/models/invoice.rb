e8b0b2cecb651452f17765d4796235b029e63bd4
karimohamed <karimohamed2002@gmail.com>
2024-02-29 00:50:32 +0200

    add methods to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,5 +4,16 @@ class Invoice < ApplicationRecord
 
     belongs_to :user, class_name: "User"
     belongs_to :supplier, class_name: "Supplier"
-    has_many :invoices_medicines, class_name: "InvoicesMedicine"
+    has_many :invoices_medicines, class_name: "InvoicesMedicine", dependent: :destroy
+
+
+    def self.create_invoice(invoice_params)
+        invoice = Invoice.new(invoice_params.except(:invoice_medicines))
+        invoice
+    end
+
+    def update_total_amount(invoice_medicines, invoice)
+        total_amount = Api::Calculations::InvoiceCalculations.calculate_total_amount(invoice_medicines)
+        invoice[:total_amount] = total_amount
+    end
 end
