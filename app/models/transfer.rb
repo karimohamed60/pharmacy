42a7b438bf6a943ebe81422f343db758f7cd9dd7
karimohamed <karimohamed2002@gmail.com>
2024-03-08 15:07:09 +0200

    added method for creating new transfer

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -3,5 +3,11 @@ class Transfer < ApplicationRecord
     self.table_name = "transfers"
 
     belongs_to :user, class_name: "User"
-    has_many :transfers_medicines, class_name: "TransfersMedicine"
+    has_many :transfers_medicines, class_name: "TransfersMedicine", dependent: :destroy
+
+
+    def self.create_transfer(transfer_params)
+        transfer = Transfer.new(transfer_params.except(:transfer_medicines))
+        transfer
+    end
 end
