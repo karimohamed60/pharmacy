c3655fb2798e163974d11c5dbf9956aeda9e343d
karimohamed <karimohamed2002@gmail.com>
2024-03-14 03:12:07 +0200

    add the method to update the got_medicine of prescription's medicine

--- app/models/prescriptions_medicine.rb
+++ app/models/prescriptions_medicine.rb
@@ -3,4 +3,16 @@ class PrescriptionsMedicine < ApplicationRecord
     self.table_name = "prescriptions_medicines"
 
     belongs_to :prescription, class_name: "Prescription"
+
+    def self.update_prescription_medicines(prescriptions_params, prescription)
+        return unless prescriptions_params.present?
+
+        prescriptions_params.each do |prescription_params|
+            prescription_medicine = PrescriptionsMedicine.find_by(id: prescription_params[:id])
+
+            prescription_medicine.update(got_medicine: prescription_params[:got_medicine]) if prescription_medicine.present?
+        end
+
+        true
+    end
 end
