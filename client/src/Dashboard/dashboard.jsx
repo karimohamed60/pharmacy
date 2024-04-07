a785438871edb1db176d2ec11b05998503d994c0
karimohamed <karimohamed2002@gmail.com>
2024-04-08 00:16:18 +0200

    added some bulk files

---
+++ client/src/Dashboard/dashboard.jsx
@@ -0,0 +1,23 @@
+
+import '../Dashboard/dashboard.css'
+import { useEffect } from 'react';
+import InventoryDashboard from '../pages/InventoryDashboard/InventoryDashboard';
+const Dashboard =()=>{
+  useEffect(() => {
+    // Remove scroll bar
+    document.body.style.overflow = 'hidden';
+
+    // Cleanup on component unmount
+    return () => {
+      document.body.style.overflow = 'visible';
+    };
+  }, []);
+return(
+<>
+<InventoryDashboard />
+</>
+
+)
+
+}
+export default Dashboard;
