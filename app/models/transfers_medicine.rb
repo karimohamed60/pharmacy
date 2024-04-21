1c2d571004d2419e04ead131925e4bb561a557b0
karimohamed <karimohamed2002@gmail.com>
2024-04-22 00:54:06 +0200

    updated the function that handles updating medicines and status of transfer

--- app/models/transfers_medicine.rb
+++ app/models/transfers_medicine.rb
@@ -19,6 +19,12 @@ class TransfersMedicine < ApplicationRecord
 
         return false unless Api::Validators::QuantityValidator.validate_quantity_in_inventory(transfers_params)
 
+        if transfer.status == 1
+            update_quantity_in_inventory = true
+        else
+            update_quantity_in_inventory = false
+        end
+
         transfers_params.each do |transfer_params|
             transfer_medicine = transfer.transfers_medicines.find_by(medicine_id: transfer_params[:medicine_id])
 
@@ -27,6 +33,18 @@ class TransfersMedicine < ApplicationRecord
                     transfer_medicine.destroy
                 else
                     transfer_medicine.update(quantity: transfer_params[:quantity])
+
+                    if update_quantity_in_inventory
+                        medicine = Medicine.find(transfer_params[:medicine_id])
+                        new_inventory_quantity = medicine.quantity_in_inventory - transfer_params[:quantity].to_i
+
+                        if new_inventory_quantity >= 0
+                            medicine.update(quantity_in_inventory: new_inventory_quantity)
+                            medicine.update(quantity_in_pharmacy: medicine.quantity_in_pharmacy + transfer_params[:quantity].to_i)
+                        else
+                            raise StandardError, "Insufficient quantity of #{medicine.name} in inventory"
+                        end
+                    end
                 end
             else
                 transfer.transfers_medicines.create(medicine_id: transfer_params[:medicine_id], quantity: transfer_params[:quantity])
