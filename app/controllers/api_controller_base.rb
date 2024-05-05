7b2387ac28ac51074247cfdf0f56068addf93854
karimohamed <karimohamed2002@gmail.com>
2024-05-05 22:13:42 +0300

    made api_controller_base to be the base of all apis

--- app/controllers/api_controller_base.rb
+++ app/controllers/api_controller_base.rb
@@ -1,4 +1,10 @@
-class ApiControllerBase < ApplicationController
+class ApiControllerBase < ActionController::API
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!
+
+    include Pundit::Authorization
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
     def render_success(data, status, additional_data = {})
         success_response = {
@@ -18,5 +24,15 @@ class ApiControllerBase < ApplicationController
             error: error
         }
         render json: error_response, status: status
+    end
+
+    protected
+
+    def user_not_authorized
+        render json: { error: "You are not authorized to perform this action." }, status: :forbidden
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :full_name, :role_id])
     end
 end
