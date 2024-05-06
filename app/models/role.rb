4cc845a0112f56b8443ed4ea7aeb89041aade908
karimohamed <karimohamed2002@gmail.com>
2024-05-06 15:09:54 +0300

    added the ability to filter users with their roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,8 @@ class Role < ApplicationRecord
     self.table_name = "roles"
 
     has_many :users, class_name: "User"
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["id", "role_name"]
+    end
 end
