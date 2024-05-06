9bd932cdbf6a8b48b8a2cf00e691441bf5a94ee7
karimohamed <karimohamed2002@gmail.com>
2024-05-06 14:53:47 +0300

    handled the destroy method while using logout endpoint

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -2,6 +2,14 @@ class Api::V1::Auth::SessionsController < Devise::SessionsController
   include RackSessionsFix
   respond_to :json
 
+  def destroy
+    if user_signed_in?
+      sign_out(current_user)
+    else
+      return false
+    end
+  end
+
   private
 
   def respond_with(resource, _opts = {})
@@ -12,9 +20,7 @@ class Api::V1::Auth::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    if current_user
-      head :no_content, status: 204
-    else
+    unless destroy
       render json: { error: "Couldn't find an active session." }, status: :unauthorized
     end
   end
